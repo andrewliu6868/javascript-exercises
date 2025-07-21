733c2981dc91c1525977ac188c483829baf4ce6f
Andrew Liu <andrewliu6867@gmail.com>
2025-07-21 13:08:08 -0700

    Completed exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    res = '';
+    arr = s.split(" ");
 
+    for(let cur = arr.length - 1; cur >= 0;  cur--){
+        temp = arr[cur];
+        for(let i = temp.length - 1; i >= 0; i--){
+            res = res + temp[i];
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
