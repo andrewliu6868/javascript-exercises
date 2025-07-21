dcaa78d16194b2efd93b69fe56105ef61282a571
Andrew Liu <andrewliu6867@gmail.com>
2025-07-21 13:00:15 -0700

    Finished exercise 7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(s , num) {
+    res = '';
+    for(let i = 0; i < num; ++i){
+        res += s;
+    }
 
+    return res;
 };
 
 // Do not edit below this line
